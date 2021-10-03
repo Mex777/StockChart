2a0a850c345198436ece76178b4214a3936e29a9
Mex777 <fologeaalexandru@gmail.com>
2021-10-03 13:16:01 +0300

    Changed the api, now you can see the price history of 5 stocks

--- controller/stock.js
+++ controller/stock.js
@@ -1,22 +1,14 @@
 var ctx = document.getElementById('myChart').getContext('2d');
-const part1 = 'https://api.polygon.io/v1/open-close/AAPL/';
-const date = new Date();
-const part2 = '?adjusted=true&apiKey=Qvah3jMPjnup44rkq7SlaDMQtn77r00E';
-var myChart = new Chart(ctx, {
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&';
+const key = 'CXQ1AQ6NSVPJB3D0';
+let myChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: [],
-    datasets: [
-      {
-        label: 'Apple Inc.',
-        data: [],
-        backgroundColor: ['white'],
-        borderColor: 'black',
-        borderWidth: 1,
-      },
-    ],
+    datasets: [],
   },
   options: {
+    responsive: true,
     scales: {
       y: {
         beginAtZero: true,
@@ -24,31 +16,44 @@ var myChart = new Chart(ctx, {
     },
   },
 });
-async function getData() {
-  //   const response = await fetch(part1 + date + part2);
-  //   const data = await response.json();
-  date.setDate(date.getDate() - 14);
-  console.log(date.toISOString().substring(0, 10));
-  for (let i = 0; i < 5; ++i) {
-    if (
-      date.getDay() != 1 &&
-      date.getDay() != 2 &&
-      date.getDay() != 0 &&
-      date.getDay() != 6
-    ) {
-      const dateInStr = date.toISOString().substring(0, 10);
-      const response = await fetch(part1 + dateInStr + part2);
-      const data = await response.json();
-      console.log(data.open, data.from);
-      myChart.data.labels.push(data.from);
-      myChart.data.datasets[0].data.push(data.open);
+const companies = ['AMZN', 'MSFT', 'FB', 'AAPL', 'TSLA'];
+
+async function getData(stockSymbol) {
+  const response = await fetch(
+    url + 'symbol=' + stockSymbol + '&apikey=' + key
+  );
+  const data = await response.json();
+  const name = data['Meta Data']['2. Symbol'];
+  const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  myChart.data.datasets.push({
+    label: name,
+    data: [],
+    backgroundColor: color,
+    borderColor: color,
+    borderWidth: 3,
+  });
+  const lastInd = myChart.data.datasets.length - 1;
+
+  let currDate = new Date();
+  currDate.setDate(currDate.getDate() - 10);
+  for (let i = 0; i < 7; ++i) {
+    if (currDate.getDay() != 0 && currDate.getDay() != 6) {
+      const dateInStr = currDate.toISOString().substring(0, 10);
+      const price = data['Time Series (Daily)'][dateInStr]['1. open'];
+      myChart.data.datasets[lastInd].data.push(price);
+      if (lastInd == 0) myChart.data.labels.push(dateInStr);
     } else {
       --i;
     }
-    date.setDate(date.getDate() + 1);
+    currDate.setDate(currDate.getDate() + 1);
   }
+}
 
+async function genData() {
+  for (let i = 0; i < companies.length; ++i) {
+    await getData(companies[i]);
+  }
   myChart.update();
 }
 
-getData();
+genData();
